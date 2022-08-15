b0dae1bab2ae5e53ee22d7071a6625da5defcb9d
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-15 16:25:44 +0300

    третье  задние,дз2

---
+++ ex2.3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("введите номер дня недели (от 1 до 7)");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<1 && number>7)
+{
+ Console.WriteLine("Пожалуйста, введите числа от 1 до 7");
+}
+else 
+{
+    if (number==1)
+    {
+        Console.WriteLine("Понедельник, не выходной");
+    }
+    else if (number==2)
+    {
+        Console.WriteLine("Вторник, не выходной");
+    }
+    else if (number==3)
+    {
+        Console.WriteLine("Среда, не выходной");
+    }
+    else if (number==4)
+    {
+        Console.WriteLine("Четверг, не выходной");
+    }
+    else if (number==5)
+    {
+        Console.WriteLine("Пятница, почти выходной");
+    }
+    else if (number==6)
+    {
+        Console.WriteLine("Суббота, выходной");
+    }
+    else if (number==7)
+    {
+        Console.WriteLine("Воскресенье, выходной");
+    }
+    
+}
