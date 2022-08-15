084b68d44d45e381fdee9e89d4f269d049e60926
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-15 16:24:08 +0300

    первое задние,дз2

---
+++ ex1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.*/
+Console.WriteLine("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<1000 && number>99) 
+{
+int secondnumber =(number%100)%10;
+Console.WriteLine( "вторая цифра числа"+" "+number+" "+"равняется"+" "+secondnumber);
+}
+else
+{
+    Console.WriteLine("пожалуйста введите трехзначное число");
+}
