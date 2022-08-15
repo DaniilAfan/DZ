88fe18df224cfd5942e42f965bcba00699e6c328
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-15 16:24:36 +0300

    второе  задние,дз2

---
+++ ex2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=0;
+if (number<99)
+{
+    Console.WriteLine ("третьей цифры нет");
+}
+else
+{
+    while (number>1000 )
+    {
+        int del =number%10;
+        number= (number-del)/10;
+        i++;
+    }
+}
+    if (i>0)
+    {
+    int thirdfigure=number%10;
+Console. WriteLine(thirdfigure);  
+    }
+
+
