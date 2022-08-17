8add5022d18576bbf541a238c065cfb7f2591c29
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-17 22:46:31 +0300

    дз 4, задача 3

---
+++ number3.4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+5 -> [1, 2, 5, 7, 19]
+
+3 -> [6, 1, 33]*/
+Console.WriteLine("введите количество элементов массива");
+int N= Convert.ToInt32(Console.ReadLine());
+int [] a;
+a = new int[N];
+Random rand = new Random();
+for (int i=0; i< a.Length; i++)
+{
+    a[i]=rand.Next(1, 21);
+}
+for (int i=0; i< a.Length; i++)
+{
+    Console.Write(a[i]+" ");
+}
