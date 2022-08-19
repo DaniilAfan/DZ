62105c66ab1051eeef05843ff44e278fdb177b8c
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-19 22:25:20 +0300

    доп задание 2

---
+++ Dop2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается*/
+string finish="exit";
+
+int i=0;
+for ( ; ;)
+{
+    i++;
+    Console.WriteLine(i);
+    Console.WriteLine("Введите exit, чтобы закончить программу");
+    String input=Console.ReadLine();
+    if (finish==input) break;
+
+
+}
