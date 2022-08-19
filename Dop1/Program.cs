47ff2db009fe8dfb4dd781212de9485cce744125
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-19 22:24:36 +0300

    доп задание 1

---
+++ Dop1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*В переменной string есть секретное сообщение, 
+во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
+string message = "секретное сообщение";
+int password = 1234;
+int trail =0;
+int accept=0;
+while (trail<3 & accept <1 )
+{
+    Console.WriteLine("Пожалуйста введите пароль, чтобы получить сообщение");
+    int input = Convert. ToInt32(Console.ReadLine());
+    if (input==password)
+    {
+        Console.WriteLine(message);
+        accept=1;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели неправильный пароль");
+        trail++;
+    }
+}
+if (trail==3)
+{
+    Console.WriteLine("Количество попыток превышено, повторите попытку позже");
+}
+
+
+
