274b891084289b684940de1f2488d3aa8129e3f3
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-25 12:15:29 +0300

    дз5, номер 2

---
+++ number2.5/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+int size= new Random().Next(1,10);
+int[] numbers= new int[size];
+Fillarray(numbers);
+int sum=0;
+void Fillarray (int[] array )
+{
+    for (int i=0; array.Length>i; i++)
+    {
+    array[i]= new Random().Next(-100,1000);
+    }
+}
+for (int i=0; i<numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+    if (i%2==1)
+    { 
+        sum+=numbers[i];
+    }
+}
+Console.WriteLine(sum);
