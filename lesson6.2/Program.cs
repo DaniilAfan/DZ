73da99a93f7d063678a582ec0d59862c8a184408
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2022-08-26 14:19:37 +0300

    Дз 6, номер 2, изм

--- lesson6.2/Program.cs
+++ lesson6.2/Program.cs
@@ -10,8 +10,20 @@ Console.WriteLine("введите k2");
 double k2=Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("введите b2");
 double b2=Convert.ToDouble(Console.ReadLine());
+if (k1==k2)
+{
+    Console.WriteLine("прямые параллельны");
+}
+else if (k1==k2 && b1==b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1<k2 || k2>k1)
+{
 double x= (b2-b1)/(k1-k2);
 double y= k1*x+b1;
 Console.WriteLine("("+x+";"+y+")");
+}
+
 
 
